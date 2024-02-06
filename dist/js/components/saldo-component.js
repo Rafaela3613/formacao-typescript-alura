6f0c4d3168770400d519476e20ed3f3c600dcd9a
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-02-06 20:49:18 -0300

    continuando projeto

--- dist/js/components/saldo-component.js
+++ dist/js/components/saldo-component.js
@@ -4,20 +4,17 @@ import Conta from "../types/Conta.js";
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
 const elementoDataAcesso = document.querySelector(".block-saldo time");
 if (elementoDataAcesso != null) {
-  elementoDataAcesso.textContent = formatarData(
-    Conta.getDataAcesso(),
-    FormatoData.DIA_SEMANA_DIA_MES_ANO
-  );
+    elementoDataAcesso.textContent = formatarData(Conta.getDataAcesso(), FormatoData.DIA_SEMANA_DIA_MES_ANO);
 }
 renderizarSaldo();
 function renderizarSaldo() {
-  if (elementoSaldo != null) {
-    elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
-  }
+    if (elementoSaldo != null) {
+        elementoSaldo.textContent = formatarMoeda(Conta.getSaldo());
+    }
 }
 const SaldoComponent = {
-  atualizar() {
-    renderizarSaldo();
-  },
+    atualizar() {
+        renderizarSaldo();
+    },
 };
 export default SaldoComponent;
